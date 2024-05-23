4a32964645ce1e132851d2de9aa04387b3725cb3
si delv <151720755+creuserr@users.noreply.github.com>
2024-05-23 12:53:41 +0800

    update download.py

--- download.py
+++ download.py
@@ -1,24 +1,45 @@
 from pytube import YouTube
 from http.server import BaseHTTPRequestHandler
+from pytube.cipher import get_throttling_function_code
+import re
+import mock
+
+def patched_throttling_plan(js: str):
+  raw_code = get_throttling_function_code(js)
+  transform_start = r"try{"
+  plan_regex = re.compile(transform_start)
+  match = plan_regex.search(raw_code)
+  transform_plan_raw = js
+  step_start = r"c\[(\d+)\]\(c\[(\d+)\](,c(\[(\d+)\]))?\)"
+  step_regex = re.compile(step_start)
+  matches = step_regex.findall(transform_plan_raw)
+  transform_steps = []
+  for match in matches:
+    if match[4] != '':
+      transform_steps.append((match[0],match[1],match[4]))
+    else:
+      transform_steps.append((match[0],match[1]))
+  return transform_steps
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     if len(self.path) < 2:
       return self.end('400 Bad Request: Incomplete Parameter', 400)
-    if self.path == "/favicon.ico":
+    if self.path == '/favicon.ico':
       self.send_response(302)
       self.send_header('Location', 'https://m.youtube.com/favicon.ico')
       return self.end_headers()
     try:
-      video = YouTube('https://youtu.be' + self.path)
-      stream = video.streams.filter(only_audio=True).first()
-      stream.download(filename='output.mp3', output_path='/tmp/')
-      self.send_response(200)
-      self.send_header('Content-type', 'audio/mp3')
-      self.end_headers()
-      with open('/tmp/output.mp3', 'rb') as f:
-        self.wfile.write(f.read())
-        return f.close()
+      with mock.patch('pytube.cipher.get_throttling_plan', patched_throttling_plan):
+        video = YouTube('https://youtu.be/' + self.path)
+        stream = video.streams.filter(only_audio=True).first()
+        stream.download(filename='output.mp3', output_path='/tmp/')
+        self.send_response(200)
+        self.send_header('Content-type', 'audio/mp3')
+        self.end_headers()
+        with open('/tmp/', 'rb') as f:
+          self.wfile.write(f.read())
+          return f.close()
     except Exception as e:
       return self.end('500 Internal Error: ' + str(e), 500)
   def end(self, text, status):
